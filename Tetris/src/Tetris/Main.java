5d8c47f83d0cbf133acc7b4c9400255d7a569418
jinhokim <jinhokim@localhost>
2021-02-02 01:27:48 +0900

    Tetris

---
+++ Tetris/src/Tetris/Main.java
@@ -0,0 +1,13 @@
+package Tetris;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class Main extends JPanel{
+	int edge = 16;
+	int width = 10;
+	int height = 20;
+	int textSquares = 2;
+
+}
