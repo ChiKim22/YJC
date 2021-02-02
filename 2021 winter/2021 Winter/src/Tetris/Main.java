8000c6519797aec47984bf4b19950b2546cf84a9
jinhokim <jinhokim@localhost>
2021-02-02 11:36:46 +0900

    add

---
+++ 2021 winter/2021 Winter/src/Tetris/Main.java
@@ -0,0 +1,18 @@
+package Tetris;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class Main extends JPanel{
+	int edge = 16;
+	int width = 10;
+	int height = 20;
+	int textSquares = 2;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
