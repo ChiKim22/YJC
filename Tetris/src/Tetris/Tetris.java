eea830a6c58c9e4d4f58a82957201ca5bc1c3763
ChiKim22 <40478315+ChiKim22@users.noreply.github.com>
2021-02-24 02:25:51 +0900

    Update Tetris.java
    Last One

--- Tetris/src/Tetris/Tetris.java
+++ Tetris/src/Tetris/Tetris.java
@@ -3,29 +3,41 @@ package Tetris;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.Timer;
 import javax.swing.event.*;
 import java.util.*;
 
 
 public class Tetris{
-		
+		public static final int WIDTH = 445, HEIGHT = 629;
 	
 		private Board board;
+		private Title title;
 		private JFrame tetris;
 		
 		public Tetris() {
 			tetris = new JFrame("Tetris");
-			tetris.setSize(445, 629);
+			tetris.setSize(WIDTH, HEIGHT);
 			tetris.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			tetris.setResizable(false);
 			tetris.setLocationRelativeTo(null); // 화면을 가운데로 출력.
 			
+			title = new Title(this);
 			board = new Board();
-			tetris.add(board);
+			
 			tetris.addKeyListener(board);
+			tetris.addKeyListener(title);
+			
+			tetris.add(title); 
 			
 			
 			tetris.setVisible(true);
+		}
+		public void gameStart() {
+			tetris.remove(title);
+			tetris.add(board);
+			tetris.revalidate();
+			
 		}
 		
 		
@@ -33,4 +45,5 @@ public class Tetris{
 			new Tetris();
 		}
 }
+
 
